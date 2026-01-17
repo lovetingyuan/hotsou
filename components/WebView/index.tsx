cc695e73f40ef74c9649e5ee5c533a9f409d9412
tingyuan <github@tingyuan.in>
2026-01-18 02:20:44 +0800

    chore

--- components/WebView/index.tsx
+++ components/WebView/index.tsx
@@ -1,114 +1,25 @@
 import { useFocusEffect } from '@react-navigation/native'
-import * as Clipboard from 'expo-clipboard'
 import React, { useCallback, useEffect } from 'react'
 import {
   ActivityIndicator,
   BackHandler,
   Image,
   Linking,
-  Modal,
   Platform,
-  ScrollView,
   Share,
-  StyleSheet,
   Text,
   ToastAndroid,
-  TouchableOpacity,
   View,
 } from 'react-native'
 import { WebView as RNWebView } from 'react-native-webview'
 
-import { useColorScheme } from '@/hooks/useColorScheme'
 import { useStore } from '@/store'
 import { getPageIcon } from '@/utils'
 
-// import { FloatingButton } from '../FloatingButton'
 import { ThemedButton } from '../ThemedButton'
-import { ThemedText } from '../ThemedText'
 import { ThemedView } from '../ThemedView'
+import InfoModal from './InfoModal'
 import { beforeLoadedInject } from './inject'
-
-function InfoModal(props: {
-  visible: boolean
-  title: string
-  url: string
-  closeModal: () => void
-}) {
-  const colorScheme = useColorScheme()
-  return (
-    <Modal
-      animationType="fade"
-      transparent={true}
-      visible={props.visible}
-      statusBarTranslucent={true}
-      onRequestClose={() => {
-        props.closeModal()
-      }}
-    >
-      <View style={styles.centeredView}>
-        <ThemedView
-          style={[styles.modalView, { shadowColor: colorScheme === 'dark' ? 'white' : 'black' }]}
-        >
-          <ThemedText style={{ fontWeight: 'bold', fontSize: 18 }} selectable>
-            {props.title || '当前页面'}
-          </ThemedText>
-          <ScrollView style={{ maxHeight: 120 }}>
-            <ThemedText selectable style={{ opacity: 0.7 }}>
-              {props.url}
-            </ThemedText>
-          </ScrollView>
-
-          <View
-            style={{
-              flexDirection: 'row',
-              gap: 10,
-              marginTop: 20,
-              justifyContent: 'flex-end',
-              flexWrap: 'wrap',
-            }}
-          >
-            <ThemedButton
-              title="浏览器打开"
-              onPress={() => {
-                Linking.openURL(props.url)
-                props.closeModal()
-              }}
-            />
-            <ThemedButton
-              title="分享"
-              type="primary"
-              onPress={() => {
-                Share.share({
-                  title: props.title,
-                  message: props.title + '\n' + props.url,
-                  url: props.url,
-                })
-                props.closeModal()
-              }}
-            />
-            <ThemedButton
-              title="复制链接"
-              type="secondary"
-              onPress={() => {
-                Clipboard.setStringAsync(props.url).then(() => {
-                  ToastAndroid.show('已复制', ToastAndroid.SHORT)
-                })
-                props.closeModal()
-              }}
-            />
-          </View>
-          <TouchableOpacity
-            onPress={props.closeModal}
-            style={{ position: 'absolute', top: 10, right: 10, padding: 5 }}
-            hitSlop={10}
-          >
-            <ThemedText style={{ fontSize: 20, color: '#999' }}>✕</ThemedText>
-          </TouchableOpacity>
-        </ThemedView>
-      </View>
-    </Modal>
-  )
-}
 
 export default function WebView(props: {
   name: string
@@ -403,28 +314,3 @@ export default function WebView(props: {
     </>
   )
 }
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-  },
-  modalView: {
-    margin: 20,
-    borderRadius: 10,
-    paddingVertical: 25,
-    paddingHorizontal: 20,
-    width: '80%',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-    flexDirection: 'column',
-    gap: 20,
-  },
-})
